a9c7a22c46cabab4c92362df641340b7a90065f8
warp <matiasfernandeztaipe@gmail.com>
2022-10-23 01:51:20 -0300

    Improved Add Problem page

--- frontend/src/views/Problems/AddProblem/AddProblem.jsx
+++ frontend/src/views/Problems/AddProblem/AddProblem.jsx
@@ -1,3 +1,5 @@
+import MDEditor from '@uiw/react-md-editor';
+
 import { createProblem } from '@services/problems'
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom'
@@ -75,209 +77,160 @@ const AddProblemForm = () => {
                 navigate('/problems')
             })
             .catch(err => {
-                    console.log(err)
-                    setErrors(err)
-                }
-            )
+                console.log(err)
+                setErrors(err)
+            })
     }
 
     return (
-        <div className="add-problem-form">
+        <>
             {errors && <div className="errors">{errors}</div>}
-            <form onSubmit={handleSubmit} noValidate>
-                <table>
-                    <tbody>
-                        <tr key="name">
-                            <td>
-                                <label htmlFor="name">
-                                    Name of the problem
-                                </label>
-                            </td>
-                            <td>
-                                <input
-                                    type="text"
-                                    name="name"
-                                    value={name}
-                                    onChange={({ target }) => setName(target.value)}
-                                />
-                            </td>
-                        </tr>
-                        <tr key="statement">
-                            <td>
-                                <label htmlFor="statement">Statement</label>
-                            </td>
-                            <td>
-                                <textarea
-                                    name="statement"
-                                    value={statement_main}
-                                    onChange={({ target }) => setStatementMain(target.value)}
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="input">Input</label>
-                            </td>
-                            <td>
-                                <textarea
-                                    name="input"
-                                    value={statement_input}
-                                    onChange={({ target }) => setStatementInput(target.value)}
-                                />
-                            </td>
-                        </tr>
-                        <tr key="output">
-                            <td>
-                                <label htmlFor="output">Output</label>
-                            </td>
-                            <td>
-                                <textarea
-                                    name="output"
-                                    value={statement_output}
-                                    onChange={({ target }) => setStatementOutput(target.value)}
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="examples">Examples</label>
-                            </td>
-                            <td>
-                                <button type="addExample" onClick={handleAddExample}>
-                                    Add Example
-                                </button>
-                            </td>
-                        </tr>
-                        {statement_examples.map((example) => (
-                            <tr key={example.id}>
-                                <td>
-                                    <label htmlFor="input">Input</label>
-                                </td>
-                                <td>
-                                    <textarea
-                                        name="input"
-                                        value={example.input}
-                                        onChange={({ target }) => {
-                                            const newExamples = [...statement_examples]
-                                            newExamples[example.id].input = target.value
-                                            setStatementExamples(newExamples)
-                                        }}
-                                    />
-                                </td>
-                                <td>
-                                    <label htmlFor="output">Output</label>
-                                </td>
-                                <td>
-                                    <textarea
-                                        name="output"
-                                        value={example.output}
-                                        onChange={({ target }) => {
-                                            const newExamples = [...statement_examples]
-                                            newExamples[example.id].output = target.value
-                                            setStatementExamples(newExamples)
-                                        }}
-                                    />
-                                </td>
-                            </tr>
+            <form onSubmit={handleSubmit} noValidate className="add-problem-form">
+                <div className="problem-form-section">
+                    <label htmlFor="name" className="section-item">
+                        Problem name
+                    </label>
+                    <input
+                        className="section-item input-name"
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="statement" className="section-item">Statement</label>
+                    <div className="section-item__md-editor">
+                        <MDEditor value={statement_main} onChange={setStatementMain} />
+                    </div>
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="input" className="section-item">Input Format</label>
+                    <div className="section-item__md-editor">
+                        <MDEditor value={statement_input} onChange={setStatementInput} />
+                    </div>
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="output" className="section-item">Output Format</label>
+                    <div className="section-item__md-editor">
+                        <MDEditor className='md-editor' value={statement_output} onChange={setStatementOutput} />
+                    </div>
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="examples" className="section-item">Examples</label>
+                    <button type="addExample" onClick={handleAddExample}>
+                        Add Example
+                    </button>
+                </div>
+                {statement_examples.map((example) => (
+                    <div key={example.id} className="problem-form-section">
+                        <label htmlFor="input" className="section-item">Input</label>
+                        <textarea
+                            className="section-item"
+                            name="input"
+                            value={example.input}
+                            onChange={({ target }) => {
+                                const newExamples = [...statement_examples]
+                                newExamples[example.id].input = target.value
+                                setStatementExamples(newExamples)
+                            }}
+                        />
+                        <label htmlFor="output" className="section-item">Output</label>
+                        <textarea
+                            className="section-item"
+                            name="output"
+                            value={example.output}
+                            onChange={({ target }) => {
+                                const newExamples = [...statement_examples]
+                                newExamples[example.id].output = target.value
+                                setStatementExamples(newExamples)
+                            }}
+                        />
+                    </div>
+                ))}
+                <div className="problem-form-section">
+                    <label htmlFor="time_limit" className="section-item">Time Limit (seconds) </label>
+                    <select
+                        className="section-item"
+                        name="time_limit"
+                        value={time_limit}
+                        onChange={({ target }) => setTimeLimit(target.value)}
+                    >
+                        {[...Array(10).keys()].map((i) => (
+                            <option key={i} value={i + 1}>
+                                {i + 1}
+                            </option>
                         ))}
-                        <tr key="time_limit">
-                            <td>
-                                <label htmlFor="time_limit">Time Limit</label>
-                            </td>
-                            <td>
-                                <input
-                                    type="number"
-                                    name="time_limit"
-                                    value={time_limit}
-                                    onChange={({ target }) => setTimeLimit(target.value)}
-                                />
-                            </td>
-                        </tr>
-                        <tr key="memory_limit">
-                            <td>
-                                <label htmlFor="memory_limit">Memory Limit</label>
-                            </td>
-                            <td>
-                                <input
-                                    type="number"
-                                    name="memory_limit"
-                                    value={memory_limit}
-                                    onChange={({ target }) => setMemoryLimit(target.value)}
-                                />
-                            </td>
-                        </tr>
-                        <tr key="custom">
-                            <td>
-                                <label htmlFor="custom">Custom Checker</label>
-                            </td>
-                            <td>
-                                <input
-                                    type="checkbox"
-                                    name="custom"
-                                    value={custom}
-                                    onChange={({ target }) => setCustom(target.checked)}
-                                />
-                            </td>
-                        </tr>
-                        <tr key="checker">
-                            <td>
-                                <label htmlFor="checker">Checker</label>
-                            </td>
-                            <td>
-                                <textarea
-                                    name="checker"
-                                    value={checker}
-                                    onChange={({ target }) => setChecker(target.value)}
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="examples">Examples</label>
-                            </td>
-                            <td>
-                                <button type="addTestCase" onClick={handleAddTestCase}>
-                                    Add Test Case
-                                </button>
-                            </td>
-                        </tr>
-                        {test_cases.map((test_case) => (
-                            <tr key={test_case.id}>
-                                <td>
-                                    <label htmlFor="input">Input</label>
-                                </td>
-                                <td>
-                                    <textarea
-                                        name="input"
-                                        value={test_case.input}
-                                        onChange={({ target }) => {
-                                            const newTestCases = [...test_cases]
-                                            newTestCases[test_case.id].input = target.value
-                                            setTestCases(newTestCases)
-                                        }}
-                                    />
-                                </td>
-                                <td>
-                                    <label htmlFor="output">Output</label>
-                                </td>
-                                <td>
-                                    <textarea
-                                        name="output"
-                                        value={test_case.output}
-                                        onChange={({ target }) => {
-                                            const newTestCases = [...test_cases]
-                                            newTestCases[test_case.id].output = target.value
-                                            setTestCases(newTestCases)
-                                        }}
-                                    />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                    </select>
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="memory_limit" className="section-item">Memory Limit (megabytes) </label>
+                    <select
+                        className="section-item"
+                        name="memory_limit"
+                        value={memory_limit}
+                        onChange={({ target }) => setMemoryLimit(target.value)}
+                    >
+                        <option value="128">128</option>
+                        <option value="256">256</option>
+                        <option value="512">512</option>
+                        <option value="1024">1024</option>
+                    </select>
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="custom" className="section-item">Custom Checker</label>
+                    <input
+                        className="section-item"
+                        type="checkbox"
+                        name="custom"
+                        value={custom}
+                        onChange={({ target }) => setCustom(target.checked)}
+                    />
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="checker" className="section-item">Checker</label>
+                    <textarea
+                        className="section-item"
+                        name="checker"
+                        value={checker}
+                        onChange={({ target }) => setChecker(target.value)}
+                    />
+                </div>
+                <div className="problem-form-section">
+                    <label htmlFor="examples" className="section-item">Examples</label>
+                    <button type="addTestCase" onClick={handleAddTestCase}>
+                        Add Test Case
+                    </button>
+                </div>
+                {test_cases.map((test_case) => (
+                    <div key={test_case.id} className="problem-form-section">
+                        <label htmlFor="input" className="section-item">Input</label>
+                        <textarea
+                            className="section-item"
+                            name="input"
+                            value={test_case.input}
+                            onChange={({ target }) => {
+                                const newTestCases = [...test_cases]
+                                newTestCases[test_case.id].input = target.value
+                                setTestCases(newTestCases)
+                            }}
+                        />
+                        <label htmlFor="output" className="section-item">Output</label>
+                        <textarea
+                            className="section-item"
+                            name="output"
+                            value={test_case.output}
+                            onChange={({ target }) => {
+                                const newTestCases = [...test_cases]
+                                newTestCases[test_case.id].output = target.value
+                                setTestCases(newTestCases)
+                            }}
+                        />
+                    </div>
+                ))}
                 <button type="submit">Submit</button>
             </form>
-        </div>
+        </>
     )
 }
 
